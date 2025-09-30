90eff3a6e2543e1e86490574023600d5f80390e1
Neky547 <nekenarab55@gmail.com>
2025-09-30 10:27:32 +0200

    class Partie, choixStarter()

--- src/main/kotlin/jeu/Partie.kt
+++ src/main/kotlin/jeu/Partie.kt
@@ -1,7 +1,11 @@
 package org.example.jeu
 
+import org.example.Aquamy
+import org.example.Flamkip
+import org.example.Springleaf
 import org.example.dresseur.Entraineur
 import org.example.monde.Zone
+import org.example.monstre.IndividuMonstre
 
 class Partie (
     var id: Int,
@@ -9,6 +13,32 @@ class Partie (
     var zone: Zone
 ){
     fun choixStarter(){
-        var monstre1 =
+        val monstre1 = IndividuMonstre(1, "springleaf", Springleaf, null, 1500.0)
+        val monstre2 = IndividuMonstre(2, "flamkip", Flamkip, null, 1500.0)
+        val monstre3 = IndividuMonstre(3, "aquamy", Aquamy, null, 1500.0)
+        var starter: IndividuMonstre? = null
+
+        //Afficher les détails des monstres
+        monstre1.afficheDetail()
+        monstre2.afficheDetail()
+        monstre3.afficheDetail()
+
+        //Afficher menu de choix
+        println("Choisissez votre monstre :\n 1.Springleaf\n 2.Flamkip\n 3.Aquamy")
+        var choixSelection = readln().toInt()
+
+        if(choixSelection !in 1..3) choixStarter()
+        if(choixSelection == 1) {starter = monstre1}
+        else if(choixSelection == 2) {starter = monstre2}
+        else { var starter = monstre3}
+
+        //Permettre à l'utilisateur de renommer le starter
+        starter!!.renommer()
+
+        //Ajouter le monstre à l'équipe du joueur
+        joueur!!.equipeMonstre.add(starter)
+        starter.entraineur = joueur
+
     }
+
 }
