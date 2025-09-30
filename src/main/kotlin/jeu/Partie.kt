a5efeb3a79964fa6f3b3a5238e9270b37236a08a
Neky547 <nekenarab55@gmail.com>
2025-09-30 09:50:06 +0200

    genereMonstre(), rencontreMonstre()

---
+++ src/main/kotlin/jeu/Partie.kt
@@ -0,0 +1,14 @@
+package org.example.jeu
+
+import org.example.dresseur.Entraineur
+import org.example.monde.Zone
+
+class Partie (
+    var id: Int,
+    var joueur: Entraineur?,
+    var zone: Zone
+){
+    fun choixStarter(){
+        var monstre1 =
+    }
+}
--- src/main/kotlin/monde/Zone.kt
+++ src/main/kotlin/monde/Zone.kt
@@ -6,9 +6,12 @@ import org.example.Flamkip
 import org.example.Galum
 import org.example.Laoumi
 import org.example.Springleaf
+import org.example.jeu.CombatMonstre
 import org.example.monstre.EspeceMonstre
 import org.example.monstre.IndividuMonstre
 import kotlin.random.Random
+import org.example.joueur
+
 
 class Zone (
     var id: Int,
@@ -18,15 +21,28 @@ class Zone (
     var zoneSuivante: Zone?, //Zone OU Null
     var zonePrecedante: Zone?,
 
-
-    //TODO rencontreMonstre()
     ){
 
-    fun genereMonstre(){
-        var especes = listOf(Springleaf, Flamkip, Aquamy, Laoumi, Bugsyface, Galum)
-        var espece = especes.random()
-        var id = Random.nextInt(1,100)
+    fun genereMonstre(): IndividuMonstre{
 
-        var monstregenere = IndividuMonstre()
+        var myEspece = especesMonstres.random()
+        var myId = Random.nextInt(1,100)
+        var myExpInit = expZone + ((-20 until 20).random()/100)*expZone
+
+
+        var monstregenere = IndividuMonstre(id = myId,nom = myEspece.nom, espece = myEspece, entraineur = null, expInit = myExpInit.toDouble())
+
+        return monstregenere
     }
+
+    fun rencontreMonstre(){
+        var monstreSauvage = genereMonstre()
+        var premierPokemon: IndividuMonstre? = null
+        for(monstre in joueur.equipeMonstre){
+            if(monstre.pv>0) premierPokemon = monstre
+        }
+        var monCombat = CombatMonstre(monstreJoueur = premierPokemon!!, monstreSauvage = monstreSauvage)
+        monCombat.lanceCombat()
+    }
+
 }
